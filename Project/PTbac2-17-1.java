a1e1afad316fd331ea49ef824c08504a46531295
NguyenDinhLinhNe <dinhlinhne2711@gmail.com>
2024-01-24 10:07:09 +0700

    bai 24-1

--- Project/PTbac2-17-1.java
+++ Project/PTbac2-17-1.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;;
 
-public class PTbac2 {
+public class PTbac2-17-1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a, b, c;
