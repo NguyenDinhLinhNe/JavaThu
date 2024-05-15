c73c975d85cd26f9ee90dd66bcc0b1e6ddebd36f
NguyenDinhLinhNe <dinhlinhne2711@gmail.com>
2024-05-15 09:10:47 +0700

    chuong 4 slide 54

---
+++ Bai_thuc_hanh/src/hashSet.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class hashSet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(6);
+        hashSet.add(3);
+        hashSet.add(4);
+        hashSet.add(1);
+        System.out.print("Cac phan tu trong hashSet la : ");
+        System.out.println(hashSet);
+        System.out.print("Nhap so muon kiem tra : ");
+        int num = sc.nextInt();
+        if(hashSet.contains(num))
+            System.out.println("Da tim thay trong HashSet");
+        else
+            System.out.println("Khong tim thay trong HashSet");
+    }
+}
