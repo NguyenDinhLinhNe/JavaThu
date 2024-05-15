16c525e2e0cfe57e7355ea6174e94d48e50f1c09
NguyenDinhLinhNe <dinhlinhne2711@gmail.com>
2024-05-15 09:35:38 +0700

    chuong 4 slide 67

---
+++ Bai_thuc_hanh/src/treeSet.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class treeSet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeSet<String> tree = new TreeSet<>();
+        tree.add("Linh");
+        tree.add("Ly");
+        tree.add("An");
+        tree.add("Tung");
+        tree.add("HP");
+        tree.add("Legion");
+
+        SortedSet<String> treeSet = new TreeSet<>();
+        treeSet = tree.tailSet("HO");
+        System.out.println(treeSet);
+    }
+}
