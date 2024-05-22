52227a3b644402190974404b06375d6d7f8d8497
NguyenDinhLinhNe <dinhlinhne2711@gmail.com>
2024-05-22 09:34:11 +0700

    NDL431_Cau1

---
+++ BaiKiemTra/src/NDL431Person.java
@@ -0,0 +1,16 @@
+package BaiKiemTra.src;
+
+import java.util.*;
+
+class Person{
+    private String name;
+    private int id;
+    private double mark;
+}
+
+public class NDL431Person {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
